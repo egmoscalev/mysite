78ee18f98616d692388982ccdf4b12c96be05479
GeorgyMoscalev <92420326+egmoscalev@users.noreply.github.com>
2026-02-03 20:13:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -o errexit
 
 pip install --upgrade pip#just in case
 
-pip install -r requirements.txt
+pip install gunicorn uvicorn #render cant find module otherwise 
 
-pip install gunicorn #render cant find module otherwise 
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
